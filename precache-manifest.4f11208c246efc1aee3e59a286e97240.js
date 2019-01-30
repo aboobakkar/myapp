343ea8ce64bb12760b4b57408ca0221d457df85c
aboobakkar <aboobakkar@webduratech.com>
2019-01-30 21:54:54 +0530

    Updates

---
+++ precache-manifest.4f11208c246efc1aee3e59a286e97240.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "219ba7a358af727edec4",
+    "url": "/myapp/static/js/main.219ba7a3.chunk.js"
+  },
+  {
+    "revision": "2be3e4c73f79c1f322fc",
+    "url": "/myapp/static/js/1.2be3e4c7.chunk.js"
+  },
+  {
+    "revision": "219ba7a358af727edec4",
+    "url": "/myapp/static/css/main.459a0ca2.chunk.css"
+  },
+  {
+    "revision": "7c1abbba41cc8aaa7ae1ceba2744ab99",
+    "url": "/myapp/index.html"
+  }
+];
